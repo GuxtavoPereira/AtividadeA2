1115ba860bd26bd76db611797b84f2035eb73b77
GuxtavoPereira <gpl200499@gmail.com>
2025-04-22 21:58:07 -0300

    finalizando o site

--- js/index.js
+++ js/index.js
@@ -1,183 +1,132 @@
+// 1. console.log() - isso ae é um teste pra ver se o site ta vivo
 const oi = () => {
   console.log("oi");
 };
 oi();
 
-// 1. getElementById
-// Esse método é usado para selecionar um elemento HTML pelo seu ID.
-// Exemplo de uso:
-const tituLo = document.getElementById("tituloPost");
-const coteudo = document.querySelector("#coteudoPost");
-tituLo.innerHTML = "Título da Postagem";
-coteudo.innerHTML =
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribuscorporis dolorum quisquam vitae libero quibusdam culpa velitrepellendus, deleniti pariatur sed amet, dolores eum fuga asperioresoptio, accusantium mollitia rem. Lorem ipsum dolor, sit ametconsectetur adipisicing elit. Ducimus quo, tenetur facere oloremquesoluta neque vitae, veniam ipsa doloribus deserunt rem? Fugiatveritatis excepturi quis dolorum iste vel perspiciatis? Quas? Loremipsum dolor sit amet consectetur adipisicing elit. Reiciendisminimasaepe odit! Necessitatibus dolorum pariatur ipsam, praesentium quieveniet laboriosam totam illo possimus quidem animi, voluptatum,fugacumque natus minima.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribuscorporis dolorum quisquam vitae libero quibusdam culpa velitrepellendus, deleniti pariatur sed amet, dolores eum fuga asperioresoptio, accusantium mollitia rem. Lorem ipsum dolor, sit ametconsectetur adipisicing elit. Ducimus quo, tenetur facere oloremquesoluta neque vitae, veniam ipsa doloribus deserunt rem? Fugiatveritatis excepturi quis dolorum iste vel perspiciatis? Quas? Loremipsum dolor sit amet consectetur adipisicing elit. Reiciendisminimasaepe odit! Necessitatibus dolorum pariatur ipsam, praesentium quieveniet laboriosam totam illo possimus quidem animi, voluptatum,fugacumque natus minima.";
+// *** MÉTODOS PARA MANIPULAÇÃO DO DOM ***
 
-const tituLo2 = document.getElementById("tituloPost2");
-const coteudo2 = document.querySelector("#coteudoPost2");
-tituLo2.innerHTML = "Título da Postagem2";
-coteudo2.innerHTML =
-  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribuscorporis dolorum quisquam vitae libero quibusdam culpa velitrepellendus, deleniti pariatur sed amet, dolores eum fuga asperioresoptio, accusantium mollitia rem. Lorem ipsum dolor, sit ametconsectetur adipisicing elit. Ducimus quo, tenetur facere oloremquesoluta neque vitae, veniam ipsa doloribus deserunt rem? Fugiatveritatis excepturi quis dolorum iste vel perspiciatis? Quas? Loremipsum dolor sit amet consectetur adipisicing elit. Reiciendisminimasaepe odit! Necessitatibus dolorum pariatur ipsam, praesentium quieveniet laboriosam totam illo possimus quidem animi, voluptatum,fugacumque natus minima.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribuscorporis dolorum quisquam vitae libero quibusdam culpa velitrepellendus, deleniti pariatur sed amet, dolores eum fuga asperioresoptio, accusantium mollitia rem. Lorem ipsum dolor, sit ametconsectetur adipisicing elit. Ducimus quo, tenetur facere oloremquesoluta neque vitae, veniam ipsa doloribus deserunt rem? Fugiatveritatis excepturi quis dolorum iste vel perspiciatis? Quas? Loremipsum dolor sit amet consectetur adipisicing elit. Reiciendisminimasaepe odit! Necessitatibus dolorum pariatur ipsam, praesentium quieveniet laboriosam totam illo possimus quidem animi, voluptatum,fugacumque natus minima.";
+// 2. getElementById() - Seleciona elemento pelo ID
+const tituloPost = document.getElementById("tituloPost");
+const conteudoPost = document.querySelector("#coteudoPost"); // querySelector usado com seletor CSS
+tituloPost.innerHTML = "Título da Postagem Exemplo"; // innerHTML define o conteúdo HTML e adiciona la dentro
+tituloPost.style.color = "teal"; // aqui eu to mudando a cor do texto do titulo da postagem, usando o estilo CSS
+conteudoPost.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste voluptate cum iure ipsum esse quis eos, placeat, debitis a eaque ullam minima, impedit aspernatur excepturi laboriosam? Deleniti veniam dolorum laboriosam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste voluptate cum iure ipsum esse quis eos, placeat, debitis a eaque ullam minima, impedit aspernatur excepturi laboriosam? Deleniti veniam dolorum laboriosam.";
 
-// const tituLo2 = document.getElementById("test");
-// tituLo2.innerHTML = "";
+// 3. querySelector() - Seleciona o primeiro elemento que corresponde ao seletor
+const tituloPost2 = document.getElementById("tituloPost2");
+const conteudoPost2 = document.querySelector("#coteudoPost2");
+tituloPost2.innerHTML = "Título da Postagem Exemplo 2";
+tituloPost2.style.color = "teal"; // aqui eu to mudando a cor do texto do titulo da postagem, usando o estilo CSS
+conteudoPost2.innerHTML = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste voluptate cum iure ipsum esse quis eos, placeat, debitis a eaque ullam minima, impedit aspernatur excepturi laboriosam? Deleniti veniam dolorum laboriosam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste voluptate cum iure ipsum esse quis eos, placeat, debitis a eaque ullam minima, impedit aspernatur excepturi laboriosam? Deleniti veniam dolorum laboriosam.";
 
-// 2. querySelector
-// Este método permite selecionar um elemento com base em um seletor CSS.
-// Exemplo de uso:
-// const titulo3 = document.querySelector(".titulo3");
-// titulo3.style.color = "blue";
-// titulo3.innerHTML = "";
-
-// 3. addEventListener
-// Utilizado para adicionar um ouvinte de eventos a um elemento.
-// Exemplo de uso:
-// const btn = document.querySelector("#btn");
-// const container = document.getElementById("container");
-
-// btn.addEventListener("click", () => {
-//   alert("JUMPSCARE FODA");
-// });
-
-// btn.addEventListener("mouseover", () => {
-//   btn.style.backgroundColor = "red";
-//   btn.innerHTML = "nao clique";
-//   btn.style.width = "150px";
-//   btn.style.height = "150px";
-//   // container.style.backgroundImage = 'url(https://colunadofla.com/wp-content/uploads/2020/07/WhatsApp-Image-2020-07-17-at-20.41.39.jpeg)'
-//   container.style.backgroundSize = "cover";
-//   container.style.backgroundPosition = "center";
-//   container.style.backgroundRepeat = "no-repeat";
-//   container.style.backgroundColor = "black";
-// });
-
-// btn.addEventListener("mouseout", () => {
-//   btn.style.backgroundColor = "black";
-//   btn.innerHTML = "";
-//   btn.style.width = "50px";
-//   btn.style.height = "50px";
-//   // container.style.backgroundImage = 'url(https://bpb-eu-w2.wpmucdn.com/blogs.ucl.ac.uk/dist/9/186/files/2017/10/800px-Gibraltar_Barbary_Macaque.jpg);'
-//   container.style.backgroundColor = "white";
-// });
-
-// btn.addEventListener("mousedown", () => {
-//   btn.style.backgroundColor = "green";
-// });
-
-// btn.addEventListener('mouseup', () => {
-//   btn.style.backgroundColor = 'yellow'
-// });
-
-// btn.textContent = "oi";
-// btn.style.backgroundColor = "black";
-// btn.style.color = "white";
-// btn.style.padding = "1%";
-// btn.style.border = "none";
-// btn.style.borderRadius = "25px";
-// btn.style.cursor = "pointer";
-// btn.style.fontSize = "16px";
-// btn.style.width = "50px";
-// btn.style.height = "50px";
-// btn.style.margin = "0.5%";
-
-// 5. createElement
-// Cria um novo elemento HTML.
-// Exemplo de uso:
-// essa função faz co que ao clicar nos botões de ação la em cima do nav, apareça uma caixinha de texto la embaixo alertando que não funciona
+// ***  MÉTODOS PARA EVENTOS ***
+// addEventListener() - é um bagui que faz com que o javascript escute o que ta acontecendo no site, ele fica ouvindo para quando algo acontecer, como um clique, ou um enevnto de submit(envio), ele executar uma função. É tipo um amigo que fica de olho no que ta rolando e te avisa quando algo acontece.
 document.addEventListener("DOMContentLoaded", function () {
-  // Elementos
+  // 4. addEventListener() - Adiciona ouvintes de eventos
   const searchBtn = document.getElementById("searchBtn");
   const userBtn = document.getElementById("userBtn");
   const sininhoBtn = document.getElementById("sininhoBtn");
 
-  // Função para mostrar mensagem
-  function mensagemPaia(message) {
+  // 5. createElement() - Cria novos elementos HTML, aqui esta sendo utilizado para criarr a div de mensagem que aparece la embaixo quando clica nos botões que eu esqueci de fazer funcionar de verdade
+  function mostrarMensagem(message) {
     const msg = document.createElement("div");
     msg.className = "message-tooltip";
+
+    // 6. textContent - Define ou obtém o texto de um nó
     msg.textContent = message;
+
+    // 7. appendChild() - Adiciona um elemento filho
     document.body.appendChild(msg);
 
-    // 4. setTimeout
-    // Esse método é usado para executar uma função após um determinado período de tempo.
-    // Exemplo de uso:
+    // 8. setTimeout() - Executa função após um tempo determinado
     setTimeout(function () {
+      // 9. remove() - Remove o elemento do DOM, remove o coiso do HTML, some com ele magicamente
       msg.remove();
     }, 3000);
   }
 
-  // Eventos para cada botão
-  searchBtn.addEventListener("click", function () {
-    mensagemPaia("Pesquisa em breve!");
-  });
+  // Configurador dos eventos para os botões de mensagem citados acima, aqui eu tô dizendo que quando clicar no botão de pesquisa, vai aparecer a mensagem "Pesquisa em breve!", e assim por diante para os outros botões tlg
+  searchBtn.addEventListener("click", () =>
+    mostrarMensagem("Pesquisa em breve!")
+  );
+  userBtn.addEventListener("click", () => mostrarMensagem("Login em breve!"));
+  sininhoBtn.addEventListener("click", () =>
+    mostrarMensagem("Notificações em breve!")
+  );
 
-  userBtn.addEventListener("click", function () {
-    mensagemPaia("Login em breve!");
-  });
-
-  sininhoBtn.addEventListener("click", function () {
-    mensagemPaia("Notificações em breve!");
-  });
+  // ***  SISTEMA DE COMENTÁRIOS ***
   const formComentarios = document.getElementById("comentario-form");
   const comentariosContainer = document.getElementById("comentarios-container");
 
   formComentarios.addEventListener("submit", function (e) {
+    // 10. preventDefault() - Impede comportamento padrão do formulário que é fazer a pagina dar um f5 e recarregar a pagina, atrapalhando o coiso
     e.preventDefault();
 
-    // Pegar valores do formulário
+    // 11. value - Obtém valores dos inputs, dos campos onde da pra escrever texto nos formulario
     const name = document.getElementById("comentario-name").value;
     const text = document.getElementById("comentario-text").value;
 
-    // Criar elementos do comentário
     const comentarioDiv = document.createElement("div");
-    // 8. setAttribute
-    // Define um atributo em um elemento.
-    // Exemplo de uso:
+    // 12. setAttribute() - Define os atributos do elemento, pode ser ua classe ou outras coisas
     comentarioDiv.setAttribute("class", "comentario");
-    // 6. createTextNode
-    // Cria um novo nó de texto.
-    // Exemplo de uso:
+
     const autorPost = document.createElement("p");
     autorPost.setAttribute("class", "comentario-autor");
-    const autorText = document.createTextNode(name);
-    autorPost.appendChild(autorText);
+
+    // 13. createTextNode() - Cria nó de texto
+    autorPost.appendChild(document.createTextNode(name));
 
     const textoAutor = document.createElement("p");
     textoAutor.setAttribute("class", "comentario-text");
-    const textNode = document.createTextNode(text);
-    // 7. appendChild
-    // Adiciona um elemento filho a um elemento pai.
-    // Exemplo de uso:
-    textoAutor.appendChild(textNode);
+    textoAutor.appendChild(document.createTextNode(text));
 
-    // Montar estrutura do comentário
     comentarioDiv.appendChild(autorPost);
     comentarioDiv.appendChild(textoAutor);
 
-    // Inserir novo comentário no topo da lista
+    // 14. firstChild - Acessa o primeiro filho do elemento
     if (comentariosContainer.firstChild) {
-      // 9. insertBefore
-      // Insere um novo elemento antes de um elemento existente.
-      // Exemplo de uso:
-      comentariosContainer.insertBefore(comentarioDiv, comentariosContainer.firstChild);
+      // 15. insertBefore() - Insere antes de outro elemento, aqui eu to dizendo que se já tiver um comentario la dentro, o novo comentario vai ser adicionado antes do primeiro comentario que ja ta la, assim fica mais organizado e os comentarios mais novos aparecem primeiro mesmo com eu esquecedo de colocar a data, fica atualizado com o mais recente primeiro
+      comentariosContainer.insertBefore(
+        comentarioDiv,
+        comentariosContainer.firstChild
+      );
     } else {
       comentariosContainer.appendChild(comentarioDiv);
     }
 
-    // Limpar formulário
+    // 16. reset() - Limpa formulário
     formComentarios.reset();
   });
 });
 
+// *** FUNÇÃO DO BOTÃO CURTIR ***
 function trocarCor() {
+  // 17. style - Manipula o CSS
+  // 18. aqui estamos selecionando o elemento do botão, e quando ce clica nele, o que acontece é a troca de cor dele e do tetxo, e quando ce tira o like, ele volta ao normal
   const botao = document.getElementById("curtir");
   const texto = document.getElementById("textoLike");
+
   if (botao.style.backgroundColor === "teal") {
-    botao.style.backgroundColor = "white"; // Volta para azul
-    botao.style.color = "black"; // Muda para branco
-    texto.innerHTML = "Curtir Postagem!"; // Muda o texto para "Curtir"
+    botao.style.backgroundColor = "white";
+    botao.style.color = "black";
+    texto.innerHTML = "Curtir Postagem!";
   } else {
     botao.style.backgroundColor = "teal";
-    botao.style.color = "white"; // Muda para vermelho
-    texto.innerHTML = "Curtido"; // Muda o texto para "Curtirdo"
-    texto.style.color = "teal"; // Muda o texto para vermelho
+    botao.style.color = "white";
+    texto.innerHTML = "Curtido";
+    texto.style.color = "teal";
   }
 }
+const curtirBtn = document.getElementById("curtir");
+
+// 19. aqui tem o evento do mouse over e o do mouse out, nós usamos ele como uma especie de hover, usei um cirador de box shadow pra fazer aparecer uma sombrazinha bonitinha quando o mouse passa por cima do botão, e quando tira o mouse de cima, some a sombra
+curtirBtn.addEventListener("mouseover", function () {
+  if (this.style.backgroundColor !== "teal") {
+    this.style.boxShadow = "0 0 5px rgba(0, 128, 128, 0.5)";
+  }
+});
+
+curtirBtn.addEventListener("mouseout", function () {
+  this.style.boxShadow = "";
+});
